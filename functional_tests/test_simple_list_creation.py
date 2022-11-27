b9dd72200b61f9899b3128afef8ea65c1d01937d
Gean <geangs1607@gmail.com>
2022-11-27 16:51:13 -0300

    Refatorando o Teste Funcional

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -6,14 +6,8 @@ from selenium.common.exceptions import WebDriverException
 
 
 MAX_WAIT = 5
-
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
+           
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self): 
     
@@ -59,20 +53,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfeita, ela vai dormir
         
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:  
-            try:
-                table = self.browser.find_element_by_id('id_list_table')  
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return  
-            except (AssertionError, WebDriverException) as e:  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise e  
-                time.sleep(0.5)
-                
-                
+    
+                 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Maria começa uma nova lista
         self.browser.get(self.live_server_url)
