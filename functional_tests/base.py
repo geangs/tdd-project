aa143df7f1b24551eb575dc17d7f8dcdccde1214
Gean <geangs1607@gmail.com>
2022-11-27 16:58:31 -0300

    Validação: Prevenindo itens vazios - PARTE 1

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -53,3 +53,13 @@ class FunctionalTest(StaticLiveServerTestCase):
             512,
             delta=10
         )
+        
+    def wait_for(self, fn):
+    start_time = time.time()
+    while True:
+        try:
+            return fn()  
+        except (AssertionError, WebDriverException) as e:
+            if time.time() - start_time > MAX_WAIT:
+                raise e
+            time.sleep(0.5)
